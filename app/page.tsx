725a1a8b96ad897dbe4f18ed143d1aa1d93c2c43
Nafis Handoko <naffisioner@gmail.com>
2023-12-26 12:26:07 +0700

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,144 @@
 import Image from 'next/image'
+import { BsPlay } from "react-icons/bs";
+import HeaderImg from '../public/img/header-img.png'
+import UIUXImg from '../public/img/uiux.png'
+import WebdevImg from '../public/img/webdev.png'
+import MobiledevImg from '../public/img/mobiledev.png'
+import ConsultantImg from '../public/img/consultant.png'
+import ServicesBg from '../public/img/services-bg.png'
+import Package1Img from '../public/img/package1.png'
+import Package2Img from '../public/img/package2.png'
+import Package3Img from '../public/img/package3.png'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    return (
+        <>
+            <nav className='container mx-auto flex flex-row items-center justify-between py-5 px-20'>
+                <a href="" className='text-primary text-5xl font-semibold'>Speedside</a>
+                <div className='flex flex-row items-center gap-20'>
+                    <ul className='flex flex-row gap-10 items-center font-semibold'>
+                        <li className='text-text-nav hover:text-primary'><a href="#">Home</a></li>
+                        <li className='text-text-nav hover:text-primary'><a href="#">Services</a></li>
+                        <li className='text-text-nav hover:text-primary'><a href="#">Package</a></li>
+                        <li className='text-text-nav hover:text-primary'><a href="#">Portfolio</a></li>
+                        <li className='text-text-nav hover:text-primary'><a href="#">About Us</a></li>
+                    </ul>
+                    <a href="#" className='bg-primary text-white rounded-full px-4 py-2'>Contact Us</a>
+                </div>
+            </nav>
+            <header className='container mx-auto flex flex-row items-center justify-between px-20 pb-20'>
+                <div className='w-1/2 flex flex-col gap-5'>
+                    <h1 className='font-bold text-6xl text-text-h1 leading-tight'>Accelerate Your Business with Speedside</h1>
+                    <svg width="487" height="34" viewBox="0 0 487 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M4 30C73.6307 10.3798 266.914 -17.0885 483 30" stroke="#FA875B" stroke-width="8" stroke-linecap="round" />
+                    </svg>
+                    <p className='text-black font-medium text-lg'>At Speedside, we accelerate innovation. Our commitment is simple: fast, affordable, and reliable solutions. Experience the power of innovation at the speed of thought.</p>
+                    <div className='flex flex-row items-center gap-7'>
+                        <a href="#" className='text-white bg-primary rounded-full px-5 py-3'>Get Started</a>
+                        <a href="#" className='flex flex-row items-center gap-3'>
+                            <div className='rounded-full w-7 h-7 flex items-center justify-center border border-black'><BsPlay /></div>
+                            <span>Learn More</span>
+                        </a>
+                    </div>
+                </div>
+                <div className='w-1/2'>
+                    <Image
+                        src={HeaderImg}
+                        alt='header image'
+                    />
+                </div>
+            </header>
+            <section className='relative py-14'>
+                <Image
+                    src={ServicesBg}
+                    alt='services bg'
+                    className='absolute top-0 left-0 -z-10'
+                />
+                <div className='container mx-auto flex flex-col gap-16 px-10'>
+                    <h2 className='text-5xl text-text-h2-purple text-center'>
+                        <span className='font-bold'>What We Do</span><br />
+                        <span className='font-light'>For Your Business</span>
+                    </h2>
+                    <div className='grid grid-cols-4 gap-7'>
+                        <div className='rounded-lg bg-white p-4 flex flex-col gap-3'>
+                            <Image
+                                src={UIUXImg}
+                                alt='uiux image'
+                            />
+                            <h4 className='font-bold text-text-purple tracking-wider'>UIUX DESIGN</h4>
+                            <p className='text-sm font-light text-text-body-gray2'>Our UI/UX design team crafts visually stunning interfaces with a focus on seamless usability.</p>
+                        </div>
+                        <div className='rounded-lg bg-white p-4 flex flex-col gap-3'>
+                            <Image
+                                src={WebdevImg}
+                                alt='webdev image'
+                            />
+                            <h4 className='font-bold text-text-purple tracking-wider'>WEB DEVELOPMENT</h4>
+                            <p className='text-sm font-light text-text-body-gray2'>Specialize in creating dynamic, responsive, and scalable platforms tailored to your business needs.</p>
+                        </div>
+                        <div className='rounded-lg bg-white p-4 flex flex-col gap-3'>
+                            <Image
+                                src={MobiledevImg}
+                                alt='mobile dev image'
+                            />
+                            <h4 className='font-bold text-text-purple tracking-wider'>MOBILE APP DEVELOPMENT</h4>
+                            <p className='text-sm font-light text-text-body-gray2'>We're committed to delivering mobile solutions that resonate with your users.</p>
+                        </div>
+                        <div className='rounded-lg bg-white p-4 flex flex-col gap-3'>
+                            <Image
+                                src={ConsultantImg}
+                                alt='uiux image'
+                            />
+                            <h4 className='font-bold text-text-purple tracking-wider'>IT CONSULTANT</h4>
+                            <p className='text-sm font-light text-text-body-gray2'>Provide strategic insights and actionable plans to fuel your digital transformation</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className='container mx-auto px-20 flex flex-col gap-7 py-20'>
+                <div className='flex flex-row items-center justify-between gap-4'>
+                    <h2 className='text-5xl font-semibold text-text-h2-black leading-snug w-1/3'>Our Support Package</h2>
+                    <p className='font-medium text-text-body-gray1 w-1/3'>At Speedside Labs, our commitment to your success doesn't end at project completion. Dive into the tailored support experience offered by our Support Package.</p>
+                    <a href="#" className='text-white bg-primary rounded-full px-5 py-3 whitespace-nowrap'>Get Started</a>
+                </div>
+                <div className='grid grid-cols-3 gap-8'>
+                    <div className='flex flex-col gap-5'>
+                        <Image
+                            src={Package1Img}
+                            alt='package1 image'
+                        />
+                        <h3 className='text-text-h3 font-semibold text-3xl'>Documentation</h3>
+                        <p className='text-text-body-gray1 font-medium'>Receive detailed and user-friendly documentation that acts as your go-to resource, guiding your team through the functionalities of your solution.</p>
+                    </div>
+                    <div className='flex flex-col gap-5'>
+                        <Image
+                            src={Package2Img}
+                            alt='package2 image'
+                        />
+                        <h3 className='text-text-h3 font-semibold text-3xl'>Maintenance & Support</h3>
+                        <p className='text-text-body-gray1 font-medium'>Our dedicated support team is at your service. We proactively address issues, conduct updates, and ensure your solution operates at peak performance.</p>
+                    </div>
+                    <div className='flex flex-col gap-5'>
+                        <Image
+                            src={Package3Img}
+                            alt='package3 image'
+                        />
+                        <h3 className='text-text-h3 font-semibold text-3xl'>Knowledge Transfer</h3>
+                        <p className='text-text-body-gray1 font-medium'>Our experts provide hands-on training, empowering your team to handle day-to-day operations confidently.</p>
+                    </div>
+                </div>
+            </section>
+            <section className='relative py-14'>
+                <div>
+                    <div className='flex flex-row items-center justify-between container mx-auto px-20'>
+                        <h2 className='text-5xl text-text-h2-purple'>
+                            <span className='font-bold'>Our Recent Work</span><br />
+                            <span className='font-light'>By Speedside Lab</span>
+                        </h2>
+                        <p className='text-text-body-purple w-2/5'>Welcome to our portfolio, where innovation meets execution. Explore a collection of projects that showcase the diversity and excellence of Speedside Labs.</p>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,33 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: '#FC8C65',
+        secondary: '#F7F0EA',
+        tertiary: '#FFC7B6',
+        text: {
+          // gray1: '#A6A6A6',
+          // gray2: '#331B3BA8',
+          // grayblue: '#0F172A'
+          nav: '#A6A6A6',
+          body: {
+            gray1: '#A6A6A6',
+            gray2: '#331B3BA8',
+            bluegray: '#0F172A',
+            purple: '#331B3B',
+          },
+          h1: '#191A15',
+          h2: {
+            purple: '#331B3B',
+            black: '#191A15'
+          },
+          h3: '#191A15',
+          h4: {
+            purple: '#331B3B',
+            bluegray: '#0F172A'
+          }
+        }
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
